f81fb5ec74072d63426f0d064e214ad1974f88cf
PerF78 <164545382+PerF78@users.noreply.github.com>
2024-05-23 20:43:40 +0300

    Add files via upload

---
+++ untitled/src/main/java/AleksandrenkoNI/chat/server/ChatServer.java
@@ -0,0 +1,14 @@
+package AleksandrenkoNI.chat.server;
+
+import java.io.IOException;
+
+public class ChatServer {
+    public static void main(String[] args) {
+        try {
+            ServerListener server = new ServerListener();
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
